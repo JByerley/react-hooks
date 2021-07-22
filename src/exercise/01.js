71132767ae38bed247a533ebe42d2ff4ddd6bb12
Jonathan Byerley <jonathan@alley.co>
2021-07-22 12:29:04 -0400

    adds e1 extra credit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName=''}) {
+  const [name, setName] = React.useState(initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
